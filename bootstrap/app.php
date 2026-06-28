2a0ab739995e22867a1f84bcb1dfe34a415f2cb8
monsco dev <muhammadvarza1905@gmail.com>
2026-06-28 16:06:46 +0700

    fixed mixed content error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // my config :
+        $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
